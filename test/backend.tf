edf14881b002063c26824af49ad667cc009280e8
Bibek Shrestha <bibek.shrestha23@gmail.com>
2021-02-13 18:51:18 -0500

    updated test codes

--- test/backend.tf
+++ test/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "64bucketlane"
-    key    = "stage/terraform.tfstate"
+    key    = "test/terraform.tfstate"
     region = "us-west-2"
     dynamodb_table = "terraformstate"
   }
